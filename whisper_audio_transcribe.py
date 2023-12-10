307bde10285a03634f5a64a10a08625d3156813e
Sri Laasya Nutheti <nutheti.laasya@gmail.com>
2023-12-10 11:06:10 -0500

    Added flow1 whisper and together API functionalities

---
+++ whisper_audio_transcribe.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key using os.getenv
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Input audio file path from interface
+file_path = "/Users/laasya/Desktop/llamahack/output.mp3"
+
+# Function takes in audio file and transcribes it
+
+
+def transcribe_audio(mp3_content):
+    # Initialize OpenAI client
+    client = OpenAI(api_key=openai_api_key)
+
+    # Open the audio file in binary mode
+    with open('audio_file.mp3', "wb") as audio_file:
+        audio_file.write(mp3_content)
+
+    # Open the audio file in binary mode
+    with open('audio_file.mp3', "rb") as audio_file:
+        # Create transcription
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="text"
+        )
+
+    # Return the transcribed text
+    return transcript
+
+
+print(transcribe_audio(file_path))
